9289f86ccac0a67c306f871fededc79a91888e49
DexterStan <46094220+DexterStan@users.noreply.github.com>
2018-12-24 15:04:21 +1300

    Module 3: Update Ranked Grade Book
    Module 3: Update Ranked Grade Book

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,5 +10,38 @@ namespace GradeBook.GradeBooks
 		{
 			Type = GradeBookType.Ranked;
 		}
-    }
+
+		public override char GetLetterGrade(double averageGrade)
+		{
+			if(Students.Count < 5)
+			{
+				throw new InvalidOperationException("Randek grading requires at least 5 students.");
+			}
+			var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+			var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+			char Result;
+
+			if(grades[threshold - 1] <= averageGrade)
+			{
+				Result = 'A';
+			}
+			else if (grades[threshold * 2 - 1] <= averageGrade)
+			{
+				Result = 'B';
+			}
+			else if (grades[threshold * 3 - 1] <= averageGrade)
+			{
+				Result = 'C';
+			}
+			else if (grades[threshold * 4 - 1] <= averageGrade)
+			{
+				Result = 'D';
+			}
+			else
+			{
+				Result = 'F';
+			}
+			return Result;
+		}
+	}
 }
